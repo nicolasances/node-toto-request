f156501a01f64f15f5fc9429939650837db87a8a
nicolas <nicolas.matteazzi@gmail.com>
2019-02-09 13:10:59 +0100

    Fixed json

--- validation/Validator.js
+++ validation/Validator.js
@@ -1,8 +1,8 @@
 
 exports.validate = (req) => {
 
-  if (req.microservice == null) return {code: 400, 'Missing "microservice" field. That should contain the host name of the microservice.'};
-  if (req.correlationId == null) return {code: 400, 'Missing "correlationId" field.'}
+  if (req.microservice == null) return {code: 400, message: 'Missing "microservice" field. That should contain the host name of the microservice.'};
+  if (req.correlationId == null) return {code: 400, message: 'Missing "correlationId" field.'}
 
   return {code: 200};
 
